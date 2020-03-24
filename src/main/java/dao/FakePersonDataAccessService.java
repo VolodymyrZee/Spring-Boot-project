cd18668afef533b52c5a5c1ac09204235abfd1a5
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-03-24 10:54:49 -0500

    dailyupdate

--- src/main/java/dao/FakePersonDataAccessService.java
+++ src/main/java/dao/FakePersonDataAccessService.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import model.Person;
 
+
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
 
 	private static List <Person> DB = new ArrayList<>();
--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -1,14 +1,24 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import dao.PersonDao;
 import model.Person;
 
+
+
+
+@Service
 public class PersonService {
 	
 	
 	private final PersonDao personDao;
 	
-	public PersonService(PersonDao personDao) {
+	
+	@Autowired    //Spring boot injects the actual service into this constructor
+	public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
 		this.personDao = personDao;
 	}
 	
