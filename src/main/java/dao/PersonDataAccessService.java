2c37b6faae796b1aaf60b90607977ac48bd2e8ab
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-04-15 15:49:33 -0500

    final update

--- src/main/java/dao/PersonDataAccessService.java
+++ src/main/java/dao/PersonDataAccessService.java
@@ -4,23 +4,40 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import model.Person;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public int insertPerson(UUID id, Person person) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public List<Person> selectAllPeople() {
+		final String sql = "SELECT id, name FROM person";
+		return jdbcTemplate.query(sql, (resultSet, i) -> {
+			
+					UUID id = UUID.fromString(resultSet.getString("id"));
+					String name = resultSet.getString("name");
+					return new Person(id, name);
+					
+		});
 		
-		return List.of(new Person(UUID.randomUUID(), "FROM POSTGRESS DB"));
+		
 	}
 
 	@Override
@@ -30,13 +47,13 @@ public class PersonDataAccessService implements PersonDao{
 
 	@Override
 	public int deletePersonById(UUID id) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public int updatePersonById(UUID id, Person person) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
